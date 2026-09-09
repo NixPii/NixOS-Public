525864e950cc42d112957cc6e44f1ab4e67cab4a
NixPii <me@nixpii.art>
2026-09-09 22:44:11 +0200

    Initial Commit to AMD branch

---
+++ README.md
@@ -0,0 +1,2 @@
+This is the AMDGPU branch of this config. AKA the default state. All NVIDIA
+related things are removed.
--- hardware-specific/hw-conf-def.nix
+++ hardware-specific/hw-conf-def.nix
@@ -3,16 +3,31 @@
   lib,
   pkgs,
   ...
-}: let
-  GPU = "AMD"; # "AMD" or "NVIDIA"
-in {
-  imports = [
-    # Conditionally import the exact hardware file path:
-    (
-      if GPU == "AMD"
-      then ./amd.nix
-      else ./nvidia.nix
-      # In testing, will fix
-    )
+}: {
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [
+      rocmPackages.clr.icd
+      #vulkan-loader
+      #vulkan-validation-layers
+      #vulkan-extension-layer
+    ];
+  };
+  hardware.amdgpu.opencl.enable = true;
+
+  hardware.amdgpu.overdrive.enable = true;
+
+  hardware.firmware = [pkgs.linux-firmware];
+
+  nixpkgs.config.rocmSupport = true;
+
+  hardware.enableRedistributableFirmware = true;
+
+  services.xserver.videoDrivers = ["amdgpu"];
+
+  environment.systemPackages = with pkgs; [
+    ollama-rocm
+    vulkan-loader
   ];
 }
