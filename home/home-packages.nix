832349d91e77b65ed8b50ca9683dcf042c678cd7
NixPii <me@nixpii.art>
2026-09-12 19:50:34 +0200

    Added wrapper for Steam Controller

--- home/home-packages.nix
+++ home/home-packages.nix
@@ -16,4 +16,13 @@
     X-XFCE-Commands=%B --gtk-single-instance=false --window-inherit-working-directory=false --working-directory=inherit;
     X-XFCE-CommandsWithParameter=%B --gtk-single-instance=false --window-inherit-working-directory=false --working-directory=inherit -e %s;
   '';
+
+  xdg.desktopEntries."Steam-BigPicture" = {
+    name = "steam-bigpicture";
+    icon = "steam";
+    exec = ''sh -c "steam -shutdown; sleep 5; pkill -9 steam; sleep 2; exec steam -bigpicture"'';
+    terminal = false;
+    type = "Application";
+  };
+  # ^ Steam Controller big picture wrapper :3
 }
